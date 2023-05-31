16ef10b511af1f1d18cd8c0469fa6ffd819adc32
Kamil Jiwa <kjiwa@users.noreply.github.com>
2023-05-31 00:11:51 -0700

    Create slice.js

---
+++ assets/js/slice.js
@@ -0,0 +1,16 @@
+// Slice Integration // Belltown Pizza, Seattle, 28357
+(function(w, d, s, o, f, js, fjs) {
+  w[o] = w[o] || function(opt) { w[o].q = w[o].q || opt };
+  (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+  js.src = f;
+  js.async = 1;
+
+  if (fjs) {
+    fjs.parentNode.insertBefore(js, fjs)
+  } else {
+    fjs = d.getElementsByTagName('head')[0];
+    fjs.appendChild(js)
+  }
+})(window, document, 'script', 'SliceWidgets', 'https://restaurant-widgets-integrations.slicelife.com/widget.js');
+SliceWidgets({ shopUuid: '60a6930d-1697-4f17-b0cd-57582ed9185a' });
+// Slice Integration // Belltown Pizza, Seattle, 28357
