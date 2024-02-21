d9d80b6fdfdc2cbb61e5f226c0daaa638d348cc2
Kamil Jiwa <kamil.jiwa@gmail.com>
2024-02-20 23:36:18 -0800

    Move configuration into a separate, validated file.

--- _tools/spirits/main.py
+++ _tools/spirits/main.py
@@ -1,35 +1,39 @@
 import argparse
 import csv
 import openpyxl
+import os.path
 import re
+import schema
+import yaml
 
-_WORKSHEET_TITLE_TO_CATEGORY_NAMES = {
-    "LiqueursCordials": "Liqueurs and Cordials",
-    "BrandyCognac": "Brandy and Cognac",
-}
-
-_WORKSHEETS_TO_SKIP = ["Order History"]
-
-_CSV_COLUMN_TITLES = ["Category", "Name", "Price"]
-
-_STRING_REPLACEMENTS = {
-    '"': "",
-    " (yr|year)": "yr",
-    "proof": " proof",
-    "WP ": "WhistlePig ",
-}
-
-
-_STRINGS_TO_LEAVE_UNFORMATTED = ["WhistlePig"]
-
-_STRINGS_TO_UPPERCASE = ["IPA", "LALO", "OFTD", "VS", "VSOP", "XO"]
+_CFG_SCHEMA = schema.Schema(
+    {
+        "exclude_worksheets": list,
+        "output_column_names": list,
+        "rename_worksheets": dict,
+        "string_substitutions": dict,
+        "strings_to_leave_unformatted": list,
+        "strings_to_uppercase": list,
+    }
+)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_filename", required=True)
-    parser.add_argument("--output_filename", required=True)
+    parser.add_argument("--config", default="spirits.yaml")
+    parser.add_argument("--input_filename", default="spirits.xlsx")
+    parser.add_argument("--output_filename", default="spirits.csv")
     return parser.parse_args()
+
+
+def load_config(config_file):
+    if not os.path.isfile(config_file):
+        raise Exception('Configuration file "{}" does not exist.'.format(config_file))
+
+    with open(config_file, "r") as f:
+        cfg = yaml.safe_load(f)
+        _CFG_SCHEMA.validate(cfg)
+        return cfg
 
 
 def is_roman_numeral(s):
@@ -40,21 +44,21 @@ def should_be_lowercase(s):
     return any(c.isdigit() for c in s)
 
 
-def should_be_uppercase(s):
-    return is_roman_numeral(s) or s.upper() in _STRINGS_TO_UPPERCASE
+def should_be_uppercase(s, cfg):
+    return is_roman_numeral(s) or s.upper() in cfg["strings_to_uppercase"]
 
 
-def format_value(value):
-    for k, v in _STRING_REPLACEMENTS.items():
+def format_value(value, cfg):
+    for k, v in cfg["string_substitutions"].items():
         value = re.sub(k, v, value, flags=re.IGNORECASE)
 
     formatted_parts = []
     for part in value.split():
-        if part in _STRINGS_TO_LEAVE_UNFORMATTED:
+        if part in cfg["strings_to_leave_unformatted"]:
             formatted_parts.append(part)
         elif should_be_lowercase(part):
             formatted_parts.append(part.lower())
-        elif should_be_uppercase(part):
+        elif should_be_uppercase(part, cfg):
             formatted_parts.append(part.upper())
         else:
             formatted_parts.append(part.title())
@@ -62,10 +66,10 @@ def format_value(value):
     return " ".join(formatted_parts)
 
 
-def parse_worksheet(worksheet):
+def parse_worksheet(worksheet, cfg):
     category = (
-        _WORKSHEET_TITLE_TO_CATEGORY_NAMES[worksheet.title]
-        if worksheet.title in _WORKSHEET_TITLE_TO_CATEGORY_NAMES
+        cfg["rename_worksheets"][worksheet.title]
+        if worksheet.title in cfg["rename_worksheets"]
         else worksheet.title
     )
 
@@ -81,30 +85,32 @@ def parse_worksheet(worksheet):
             continue
 
         price = int(row[2].value) if isinstance(row[2].value, float) else ""
-        values.append([category, format_value(value), price])
+        values.append([category, format_value(value, cfg), price])
 
     return values
 
 
-def write_values_to_csv(filename, values):
+def write_values_to_csv(filename, values, cfg):
     with open(filename, "w") as f:
         writer = csv.writer(f)
-        writer.writerow(_CSV_COLUMN_TITLES)
+        writer.writerow(cfg["output_column_names"])
         for row in values:
             writer.writerow(row)
 
 
 def main():
     args = parse_args()
+    cfg = load_config(args.config)
+
     wb = openpyxl.load_workbook(args.input_filename)
     values = []
     for i in wb.sheetnames:
-        if i in _WORKSHEETS_TO_SKIP:
+        if i in cfg["exclude_worksheets"]:
             continue
 
-        values += parse_worksheet(wb[i])
+        values += parse_worksheet(wb[i], cfg)
 
-    write_values_to_csv(args.output_filename, sorted(values))
+    write_values_to_csv(args.output_filename, sorted(values), cfg)
 
 
 if __name__ == "__main__":
--- _tools/spirits/run.sh
+++ _tools/spirits/run.sh
@@ -6,6 +6,7 @@ python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 black main.py
+yamllint spirits.yaml
 python main.py $@
 deactivate
 rm -rf env
